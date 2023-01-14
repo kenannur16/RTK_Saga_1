04d8076545931201f067e7139e21093ea65efa34
kenannur16 <kenannur16@gmail.com>
2023-01-14 23:48:13 +0300

    fetchin data from url via using redux saga

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getCatsFetch } from "./catState";
+import "./App.css";
 
 function App() {
+  const cats = useSelector((state) => state.cats.cats);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, [dispatch]);
+  console.log(cats);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CAT SPECIES GALLERY</h1>
+      <p>
+        Images of the different species. Lots of cats for your viewing plesure
+      </p>
+      <hr />
+      <div className="Gallery">
+        {cats.map(cat => 
+          <div key={cat.id} className="row">
+            <div className="column column-left">
+              <img
+                alt={cat.name}
+                src={"https://picsum.photos/200"}
+                width="200"
+                height="200"
+              />
+            </div>
+
+            <div className="column column-right">
+              <h2>{cat.name}</h2>
+              <h5>temperment: {cat.temperment}</h5>
+              <p>{cat.description}</p>
+            </div>
+          </div>
+        )}
+      </div>
+      <button>
+        VIEW MORE CATS
+      </button>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Provider } from 'react-redux';
+import createSagaMiddleware from '@redux-saga/core';
+import { configureStore } from '@reduxjs/toolkit';
+import catsReducer from './catState';
+import catSaga from './catSaga';
+
+const saga = createSagaMiddleware();
+const store = configureStore({
+  reducer: {
+    cats: catsReducer
+  },
+  middleware: [saga]
+});
+
+saga.run(catSaga);
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
